4089f45b57a26750ba599f53ccc78787cd1425cc
mario-69-mario <mario-69-mario@nel.id.au>
2023-07-13 03:47:12 +0000

    update

--- app/setup.sh
+++ app/setup.sh
@@ -5,4 +5,7 @@ sudo apt install pip
 
 #sudo /usr/local/bin/python -m pip install --upgrade pip
 sudo pip install --no-cache-dir --upgrade -r requirements.txt --target=/app/appRequirements
-sudo export PYTHONPATH=/app/appRequirements
+#export PYTHONPATH=/app/appRequirements
+
+git config --global user.name "mario-69-mario"
+git config --global user.email mario-69-mario@nel.id.au
