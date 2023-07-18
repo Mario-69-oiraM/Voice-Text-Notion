d824e4af8130e77bebd23a16672cd6f190d4f9a8
mario-69-mario <mario-69-mario@nel.id.au>
2023-07-18 04:13:36 +0000

    moving to docker

--- docker/b.sh
+++ docker/b.sh
@@ -1,7 +1,7 @@
 docker ps
 
-docker stop notionscheduleler
-docker rm notionscheduleler 
+docker stop voice-text-notion
+docker rm voice-text-notion
 
 #create cron tab file 
 echo "# Auto build from buildContainer script"  > crontab
@@ -12,4 +12,5 @@ echo " " >> crontab
 echo "# Run automation every min" >> crontab
 echo "*/10 * * * * bash /app/runSchedule.sh >> /var/log/myjob.log 2>&1" >> crontab
 
-docker build -t my-python --build-arg NOTION_TOKEN=$NOTION_TOKEN --build-arg NOTION_TOKEN_heartbeat=$NOTION_TOKEN_heartbeat .
+docker build -t voice-text-notion --build-arg audioPath=$audioPath --build-arg textPath=$textPath --build-arg dataPath=$dataPath --build-arg wavPath=$wavPath --build-arg processedAudioPath=$processedAudioPath --build-arg openai_api_key=$openai_api_key --build-arg NotionDB=$NotionDB --build-arg Notion_secret=$Notion_secret --build-arg failedPath=$failedPath --build-arg openai_api_key=$openai_api_key --build-arg zprocessedAudioPath=$processedAudioPath --build-arg openai_api_key=$openai_api_key --build-arg NotionDB=$NotionDB --build-arg Notion_secret=$Notion_secret .
+
