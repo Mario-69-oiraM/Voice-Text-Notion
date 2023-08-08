feb0ba2c44c0db1ed22631bc1eb8468c7fa59120
mario-69-mario <mario-69-mario@nel.id.au>
2023-08-08 14:36:29 +0000

    update

--- docker/r.sh
+++ docker/r.sh
@@ -1,5 +1,5 @@
 docker stop voice-text-notion
 docker rm voice-text-notion
 
-docker run -d -t -v /shared:/shared --name voice-text-notion voice-text-notion
+docker run -d -t -v /shared:/shared --restart unless-stopped --name voice-text-notion voice-text-notion
 
